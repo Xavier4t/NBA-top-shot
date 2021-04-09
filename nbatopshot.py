497ec9ffa2936c02f1f4201969662586497c07d5
Francisco J. Perez <javi4t@gmail.com>
2021-04-09 19:05:24 -0400

    added browser selection

--- nbatopshot.py
+++ nbatopshot.py
@@ -8,9 +8,11 @@ from bs4 import BeautifulSoup as bs
 
 # Load env to retrieve hidden variables
 load_dotenv()
-executable_path=os.getenv('executable_path')
+chrome_path=os.getenv('chromedriver')
+firefox_path=os.getenv('geckodriver')
 User=os.getenv('funds_User')
 Password=os.getenv('funds_Pass')
+exPath={1: chrome_path, 2: firefox_path}
 
 # Create a new folder each time the script is run, named with the current date and time
 seconds = time.time()
@@ -30,24 +32,50 @@ except:
 loginURL='https://www.addmorefunds.com/login/'
 fundsURL='https://www.addmorefunds.com/nft/nba-top-shot/'
 
-# Function to initilize browser
-def initBrowser(exPath, outputPath, headless=False):
-    exPath = {"executable_path": exPath}
-    options = webdriver.ChromeOptions()
+# Function to initilize Chrome browser
+def initChrome(executablePath, outputPath):
+    expath = {"executable_path": executablePath}
+    options = webdriver.ChrohOptions()
     prefs = {
         "download.default_directory" : outputPath,
         "download.directory_upgrade": "true",
         "download.prompt_for_download": "false",
         "disable-popup-blocking": "true"
-
     }
     options.add_experimental_option("prefs", prefs)
     options.add_argument("--disable-infobars")
-    options.add_argument("--no-sandbox");
     options.add_argument("--start-maximized")
     
-    return Browser("chrome", **exPath, headless=headless, options=options)
+    return Browser("chrome", **expath, headless=False, options=options)
 
+# Function to initilize Firefox browser
+def initFirefox(executablePath, outputPath):
+    expath = {"executable_path": executablePath}
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.dir", outputPath)
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", 
+                           "text/plain,text/csv,application/csv,application/download,application/octet-stream")
+    profile.set_preference("browser.download.manager.showAlertOnComplete", False)
+    profile.set_preference("browser.download.panel.shown", False)
+    profile.update_preferences()
+    return Browser("firefox", profile, **expath, headless=False)
+
+# Function to select the browser to initialize
+def initBrowser(exPath, downloadFolder):
+    browserType = {1: "Chrome", 2: "Firefox"}
+    driver = int(input('Which browser would you like to use (1: Chrome, 2: Firefox)? '))
+    if driver==1:
+        browser=initChrome(exPath[1], downloadFolder)
+        return browser
+    elif driver==2:
+        browser=initFirefox(exPath[2], downloadFolder)
+        return browser
+    else:
+        print('\n\nPlease enter the right option (1: Chrome, 2: Firefox). ')
+        initBrowser(exPath, downloadFolder)
+        
 # Log In function
 def fundsLogin(url,browser):
     browser.visit(url)
@@ -63,6 +91,7 @@ def DownlowadCSV(browser, numpages):
     target3='button[aria-label="Close"]'
     advancepage=browser.find_by_tag('div[class="text-left col-4"]').find_by_tag('button[class="btn btn-secondary"]').first
     for page in range(1,numpages):
+        print(f'\nStart downloads in page {page}\n', flush=True)
         if browser.is_element_visible_by_css(target1, wait_time=25):
             sold_history=browser.find_by_css(target1)
             for s in sold_history:
@@ -72,12 +101,12 @@ def DownlowadCSV(browser, numpages):
                         try:
                             export=browser.find_by_css(target2)
                             export.mouse_over()
-#                             export.click()
+                            export.click()
                             time.sleep(2)
                         except Exception as e:
-                            print(f'"Export" element error on page {page}.\n{e}')
+                            print(f'"Export" element error on page {page}.\n{e}', flush=True)
                     else:
-                        print(f'"Export" element not visible on page {page}.\n')
+                        print(f'"Export" element not visible on page {page}.\n', flush=True)
                     if browser.is_element_visible_by_css(target3, wait_time=25):
                         try: 
                             c=browser.find_by_css(target3)
@@ -85,21 +114,21 @@ def DownlowadCSV(browser, numpages):
                             c.click()
                             time.sleep(1)
                         except Exception as e:
-                            print(f'"Close element error on page {page}:\n{e}')
+                            print(f'"Close element error on page {page}:\n{e}', flush=True)
                     else:
-                        print(f'"Close" element not visible on page {page}.\n')
+                        print(f'"Close" element not visible on page {page}.\n', flush=True)
                                
                 except Exception as e:   
-                    print(f'"Sold History" element error on page {page}.\n{e}')
+                    print(f'"Sold History" element error on page {page}.\n{e}', flush=True)
                         
-                                  
+            print(f'Finished downloads in page {page}\n', flush=True)                      
             advancepage.click()            
             time.sleep(1)
             browser.execute_script("window.scrollTo(0, 0);")
             if (page % 2) == 0:
-                time.sleep(6)
+                time.sleep(4)
             else:
-                time.sleep(3)
+                time.sleep(2)
 
 # Function to find the number of pages on nbatopshop
 def Page(browser):
@@ -110,8 +139,8 @@ def Page(browser):
     return numpages
     
 # Function to initialize the scrap
-def NBATopShot(url1, url2):
-    browser = initBrowser(executable_path, outputPath, False)
+def NBATopShot(url1, url2, exPath):
+    browser = initBrowser(exPath, outputPath)
     fundsLogin(url1, browser)
     time.sleep(1)
     browser.visit(url2)
@@ -124,13 +153,13 @@ def NBATopShot(url1, url2):
     totalseconds=end-start
     total=time.gmtime(totalseconds)       
     time.sleep(1)
-    js = f'var message; message= "Download Complete in {total[3] hours {total[4]} minutes {total[5]} seconds"; window.alert(message);'
+    js = f'var message; message= "Download Complete in {total[3]} hours {total[4]} minutes {total[5]} seconds", window.alert(message);'
     time.sleep(1)           
     browser.execute_script(js)
 # Initialize the script
 
 if __name__ == "__main__":
     try:
-        NBATopShot(loginURL, fundsURL)
+        NBATopShot(loginURL, fundsURL, exPath)
     except Exception as e:
-        print(e)  
+        print(e, flush=True)  
